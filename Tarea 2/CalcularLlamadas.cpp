d57629e8fc5d04f2d918288285b1e2738bda90e5
Luis Josue Santana Perez <josuesantana17@gmail.com>
2024-09-06 20:47:08 -0400

    feat: Add program to calculate call charges based on call type and duration

---
+++ Tarea 2/CalcularLlamadas.cpp
@@ -0,0 +1,76 @@
+/*
+ * Curso: INF-411
+ * Nombre: Luis Josue Santana Perez
+ * Matricula: 100218446
+ * Fecha: 6/9/2024
+ * Programa que calcula la taza del dia de pesos Dominicano a Dollar
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    const float COSTO_LLAMADA_LOCAL = 0.5;
+    const float COSTO_LLAMADA_INTERNACIONAL = 1.25;
+    int menuTipoDeLlamada = 0;
+    char tipoDeLlamada[30];
+    int minutosLladamas = 0;
+    float montosACobrarLlamadas = 0.0;
+
+    do
+    {
+        while (true)
+        {
+            printf("Ingrese los minutos para calcular el monto a cobrar:");
+            if (scanf("%d", &minutosLladamas) == 1)
+            {
+                break;
+            }
+            else
+            {
+                printf("Error al leer la cantidad de minutos. Por favor, intente de nuevo.\n");
+                while (getchar() != '\n')
+                    ;
+            }
+        }
+
+        while (true)
+        {
+            printf("Ingrese el tipo de llamada a calcular\n");
+            printf("0. Salir\n");
+            printf("1. Llamadas locales\n");
+            printf("2. Llamadas internacionales\n");
+            printf("Opcion: ");
+            if (scanf("%d", &menuTipoDeLlamada) == 1 && (menuTipoDeLlamada >= 0 && menuTipoDeLlamada <= 2))
+            {
+                break;
+            }
+            else
+            {
+                printf("Error al leer la opcion. Por favor, intente de nuevo.\n");
+                while (getchar() != '\n')
+                    ;
+            }
+        }
+        switch (menuTipoDeLlamada)
+        {
+        case 0:
+            return 0;
+            break;
+        case 1:
+            strcpy(tipoDeLlamada, "Llamadas locales");
+            montosACobrarLlamadas = (COSTO_LLAMADA_LOCAL * minutosLladamas);
+            break;
+
+        case 2:
+            strcpy(tipoDeLlamada, "Llamadas internacionales");
+            montosACobrarLlamadas = (COSTO_LLAMADA_INTERNACIONAL * minutosLladamas);
+            break;
+        }
+
+        printf("El monto a cobrar por %s es: RD$ %.2f\n", tipoDeLlamada, montosACobrarLlamadas);
+    } while (true);
+
+    return 0;
+}
